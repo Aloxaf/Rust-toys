273a9d90bb707e53a41c4f7e6ef6518c2c3178aa
Aloxaf <bailong104@gmail.com>
2018-12-14 12:31:02 +0800

    优化代码

--- img2ascii/src/main.rs
+++ img2ascii/src/main.rs
@@ -1,20 +1,32 @@
 use clap::{load_yaml, value_t, App};
-use image::{imageops, GenericImage};
+use image::{imageops, GenericImageView, ImageError};
 use img2ascii::image2ascii;
-use std::{fs::File, io::prelude::*, path::Path, process};
+use std::fs::OpenOptions;
+use std::io::{self, prelude::*};
+
+enum Error {
+    OutputError(io::Error),
+    InputError(ImageError),
+}
 
 fn main() {
+    match run() {
+        Err(Error::OutputError(err)) => eprintln!("output error:\n  {}", err),
+        Err(Error::InputError(err)) => eprintln!("input error:\n  {}", err),
+        Ok(_) => std::process::exit(0),
+    }
+    std::process::exit(1);
+}
+
+fn run() -> Result<(), Error> {
     let yaml = load_yaml!("../cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let inputfile = matches.value_of("filename").unwrap();
-    let width: u32 = value_t!(matches.value_of("width"), u32).unwrap_or(0);
-    let height: u32 = value_t!(matches.value_of("height"), u32).unwrap_or(0);
+    let input = matches.value_of("filename").unwrap();
+    let width = value_t!(matches.value_of("width"), u32).unwrap_or(0);
+    let height = value_t!(matches.value_of("height"), u32).unwrap_or(0);
 
-    let image = image::open(inputfile).unwrap_or_else(|e| {
-        eprintln!("failed to open {}:\n  {}", inputfile, e);
-        process::exit(1);
-    });
+    let image = image::open(input).map_err(Error::InputError)?;
 
     let (img_w, img_h) = image.dimensions();
 
@@ -26,21 +38,21 @@ fn main() {
     };
 
     let image = image.resize(width, height, imageops::Nearest).to_luma();
-    let output = image2ascii(&image);
 
-    if let Some(outputfile) = matches.value_of("output") {
-        if Path::new(outputfile).exists() && matches.occurrences_of("force") == 0 {
-            eprintln!("failed to write {}:\n  file already exists!", outputfile);
-            process::exit(1);
+    let mut output = match matches.value_of("output") {
+        Some(name) => {
+            let file = OpenOptions::new()
+                .write(true)
+                .create(matches.occurrences_of("force") == 0)
+                .open(name)
+                .map_err(Error::OutputError)?;
+            Ok(Box::new(file) as Box<Write>)
         }
-        let mut f = File::create(outputfile).unwrap_or_else(|e| {
-            eprintln!("failed to write {}:\n   {}", outputfile, e);
-            process::exit(1);
-        });
-        write!(f, "{}", output.join("\n"));
-    } else {
-        for s in output {
-            println!("{}", s);
-        }
+        None => Ok(Box::new(io::stdout()) as Box<Write>),
+    }?;
+
+    for s in image2ascii(&image) {
+        write!(output, "{}", s).map_err(Error::OutputError)?;
     }
+    Ok(())
 }
